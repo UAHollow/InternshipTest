4d8b614a5e5aff026e03b7201047c445db139708
UAHollow <uahollow@gmail.com>
2018-06-06 18:26:24 +0300

    added local fields "knowledge" and "name" and their get and set methods to Student class along with implementation of constructor.

--- src/person/Student.java
+++ src/person/Student.java
@@ -3,11 +3,26 @@ package person;
 import person.consciousness.Knowledge;
 
 public class Student {
+    private String name;
+    private Knowledge knowledge;
+
     public Student(String name) {
-        //TODO: Implementation is needed
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Knowledge getKnowledge() {
+        return knowledge;
     }
 
     public void setKnowledge(Knowledge knowledge) {
-        //TODO: Implementation is needed
+        this.knowledge = knowledge;
     }
 }
